48f9d6d8ea34128a8ad77691a4f3cd474223e89a
Hanxiao Mao <hanxmao@hotmail.com>
2022-06-04 05:30:16 +0000

    drawline functions added

---
+++ box_it_script/answer.js
@@ -0,0 +1,40 @@
+//user input as array
+const input = []
+let i =2
+while (process.argv[i]){
+    input.push(process.argv[i])
+    i++
+} 
+
+//find largest element length of the input array
+let larLength=0
+for (i=0;i<input.length;i++){
+    if(input[i].length>larLength){
+        larLength = input[i].length
+    }
+}
+
+// console.log(input)
+// console.log(larLength)  
+
+function drawLine(num){
+    return '━'.repeat(num)
+}
+
+function drawTopBorder(num){
+    return '┍'+'━'.repeat(num)+'┑'
+}
+
+function drawMidBorder(num){
+    return '┝'+'━'.repeat(num)+'┥'
+}
+
+function drawBotBorder(num){
+    return '┕'+'━'.repeat(num)+'┙'
+}
+
+function drawBarsAround(str){
+    return '┃'+ str +''.repeat(larLength-str.length) + '┃'
+}
+
+
